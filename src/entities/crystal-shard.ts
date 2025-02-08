ba089e296f07d5013c439093c0772cd5e659333d
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2025-02-08 10:28:33 -0500

    fix shard bug

--- src/entities/crystal-shard.ts
+++ src/entities/crystal-shard.ts
@@ -4,10 +4,11 @@ import Game from "../game";
 
 export default class CrystalShard extends Entity {
   visible = false
+  intervalId: number = -1
 
   constructor(game: Game) {
     super(game, { ch: "*", fg: "purple" });
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       let multiplier = RNG.getUniform() * 150 + 10
       let colorString = RNG.getItem(["darkgray", "blue"])
       let color = Color.add(Color.fromString(colorString!), [multiplier, multiplier, multiplier])
@@ -21,6 +22,7 @@ export default class CrystalShard extends Entity {
     const xy = this.getXY()
     this.getLevel()!.textBuffer.displayBox("A crystal shard! I can feel it buzzing with energy. My equipment seems to respond as well...", () => {
       this.remove()
+      clearInterval(this.intervalId)
       // TODO power up
     })
     return false;
