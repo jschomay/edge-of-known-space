4ddda398e749ee4d8255a80f95bec765f4fb36e6
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2025-02-16 14:42:05 -0500

    include fallback mp3 for safari and ie browsers

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,34 @@ window.addEventListener("load", handleEvent);
 function handleEvent(e: Event) {
   switch (e.type) {
     case "load":
-      loadFile("bg.ogg").then((track) => {
-        document.querySelector("p")?.remove()
-        new Game(() => startBgMusic(track))
-      });
+      launch("bg.ogg").catch((e) => {
+        console.error(e);
+        if (e.message.includes("Decoding")) {
+          console.error(e)
+          launch("bg.mp3").catch((e) => showError(e.message))
+        } else {
+          showError(e.message)
+        }
+      })
   }
   window.removeEventListener("load", handleEvent);
 }
 
+function showError(msg) {
+  document.body.innerHTML = `
+    <p>I'm sorry, there's been some trouble loading the sound file. If you see this, please try running it in a different browser.  Also, you can leave a comment with the error: "${msg}"</p>
+    `
+}
 
 
 const audioCtx = new AudioContext();
 
+async function launch(file) {
+  return loadFile(file).then((track) => {
+    document.querySelector("p")?.remove()
+    new Game(() => startBgMusic(track))
+  })
+}
 
 async function loadFile(filepath) {
   const response = await fetch(filepath);
